dd3396554ec6f8dafc330c61fb8d2bfb1bd79218
bfid111 <bfid1111@gmail.com>
2016-09-08 17:51:35 -0400

    AddItem

---
+++ ItemReminder/AddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddViewController.swift
+//  ItemReminder
+//
+//  Created by Youngmin Son on 9/8/16.
+//  Copyright © 2016 Youngmin Son. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
